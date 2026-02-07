4e42d2d4e5c6dae91dd64467059876d7493250ba
Karl Ding <karlding@users.noreply.github.com>
2026-02-07 13:36:44 -0800

    Remove unused electron-webpack files

--- src/main/preload.ts
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('api', {
-  getSongData: (sourceSkid: number, sourceSequenceNbr: number) =>
-    ipcRenderer.invoke('db:getSongData', sourceSkid, sourceSequenceNbr),
-});
--- src/renderer/index.ts
+++
@@ -1,4 +0,0 @@
-import '../renderer';
-
-const mod = module as NodeModule & { hot?: { accept(): void } };
-if (mod.hot) mod.hot.accept();
